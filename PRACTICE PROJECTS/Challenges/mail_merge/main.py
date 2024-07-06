eff92056b7fecd83294c6ab9cb82b71ceb2d4da5
Robert <anutarobertmarian@gmail.com>
2024-07-06 14:11:45 +0100

    Email invitations challenge, create an email text template for every name in my list

--- PRACTICE PROJECTS/Challenges/mail_merge/main.py
+++ PRACTICE PROJECTS/Challenges/mail_merge/main.py
@@ -1,11 +1,11 @@
 with open("invited_names.txt") as f:
     names = f.readlines()
     names = [name.strip() for name in names]
-    for name in names:
 
-        with open("starting_letter.txt", mode="r+") as f:
-            letter = f.read()
-            letter = letter.replace("[name]", name)
+for name in names:
+    with open("starting_letter.txt", mode="r+") as f:
+        letter = f.read()
+        letter = letter.replace("[name]", name)
 
-            with open(f"./ReadyToSend/letter_to_{name}", mode= "w") as f:
-                f.write(letter)
+    with open(f"./ReadyToSend/letter_to_{name}", mode= "w") as f:
+        f.write(letter)
