c55e1671610decc4f87c88df880fa9e1124a55b7
Robert <anutarobertmarian@gmail.com>
2025-04-26 13:51:59 +0100

    Create data base structure and show in index the books and also link to the books details as individual page

--- django_projects/book_store_project/book_outlet/views.py
+++ django_projects/book_store_project/book_outlet/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.db.models import Avg
+
+from .models import Book
+
 
 # Create your views here.
+def index(request):
+    # order by db column with '-' is discending order
+    books = Book.objects.all().order_by("-title")
+    num_books = books.count()
+    avg_rating = books.aggregate(Avg("rating"))  # rating__avg
+
+    return render(
+        request,
+        "book_outlet/index.html",
+        {
+            "books": books,
+            "total_number_of_books": num_books,
+            "average_rating": avg_rating,
+        },
+    )
+
+
+def book_detail(request, slug):
+
+    book = get_object_or_404(Book, slug=slug)
+
+    return render(
+        request,
+        "book_outlet/book_detail.html",
+        {
+            "title": book.title,
+            "author": book.author,
+            "rating": book.rating,
+            "is_bestseller": book.is_bestselling,
+        },
+    )
