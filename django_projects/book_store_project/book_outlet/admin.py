2a9e6efb195230c90a1b6763e58c65ea490646bd
Robert <anutarobertmarian@gmail.com>
2025-05-11 05:54:32 +0100

    Create different types of relations between the models

--- django_projects/book_store_project/book_outlet/admin.py
+++ django_projects/book_store_project/book_outlet/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Book, Author, Address, Country
+
 
 # Register your models here.
+class BookAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+    list_display = ("title", "author", "rating")
+    search_fields = ("title", "author")
+    list_filter = ("author", "rating")
+
+
+admin.site.register(Book, BookAdmin)
+admin.site.register(Author)
+admin.site.register(Address)
+admin.site.register(Country)
