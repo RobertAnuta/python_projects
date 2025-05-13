9004bdc2c3b5fbc2e8307cd46dcd506f708ddf50
Robert <anutarobertmarian@gmail.com>
2025-05-13 06:42:59 +0100

    Define the models for Posts and Author

--- django_projects/my_site_project/blog/models.py
+++ django_projects/my_site_project/blog/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Author(models.Model):
+    pass
+
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=60)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="posts")
+    date = models.DateField()
+    image = models.ImageField()
+    excerpt = models.CharField(max_length=200)
+    content = models.CharField(max_length=1000)
+    slug = models.SlugField(default="", null=False, db_index=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("post-detail", args=[self.slug])
